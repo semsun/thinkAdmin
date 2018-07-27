c6ecf3f66427c0825ca330f3bb7f3f973db70858
SunYongQian <semsun@warickmedical.com>
2018-07-27 21:40:08 +0800

    心跳接口接入消息系统

--- application/api/controller/Heartbeat.php
+++ application/api/controller/Heartbeat.php
@@ -4,40 +4,46 @@ namespace app\api\controller;
 use controller\BasicApi;
 use service\LogService;
 use app\api\service\FileRecordService;
+use app\api\model\MessageCache;
 
 class Heartbeat extends BasicApi
 {
 
     public function index()
     {
-	$value = "123456";
-	$value = base64_encode($value);
-	$data = ["message" => $value];
-	return $this->success("OK", $value, 0);
+		$getData = $this->request->get();
+		if( isset($getData["mechine_no"]) ) {
+			$mechine_no = $getData["mechine_no"];
+		} else {
+			$this->error("lost param mechine_no", null, 401);
+		}
+		$msgList = MessageCache::getMessages($mechine_no);
+		
+		return $this->success("OK", $msgList, 0);
     }
 
     public function saveFileRecord() {
-	$data = FileRecordService::createFileRecord("123", "456", "filePath");
-	return implode("_", $data);
+		$data = FileRecordService::createFileRecord("123", "456", "filePath");
+		return implode("_", $data);
     }
 
     public function queryFileRecord() {
-	$where = ["file_no"=>"1"];
-	//$where = [];
-	$ret = FileRecordService::queryFileRecord($where);
-	$str = json_encode($ret);
-	return $str;
+		$where = ["file_no"=>"1"];
+		//$where = [];
+		$ret = FileRecordService::queryFileRecord($where);
+		$str = json_encode($ret);
+		return $str;
     }
 
     public function updateFileRecord() {
-	$data = ["upload_time" => date("Y-m-d H:i:s")];
+		$data = ["upload_time" => date("Y-m-d H:i:s")];
 
-	$ret = FileRecordService::updateFileRecord("1", $data);
-	
-	if( $ret ) {
-		return "OK";
-	}
-	return "Failed";
+		$ret = FileRecordService::updateFileRecord("1", $data);
+		
+		if( $ret ) {
+			return "OK";
+		}
+		return "Failed";
     }
 
 }
